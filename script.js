e9ce9ac20d516346f384becb574d0563490c51e1
muulinha <lucasfl.araujo@hotmail.com>
2022-11-29 13:50:06 +0800

    added comments to the js file

--- script.js
+++ script.js
@@ -7,29 +7,35 @@ var numbs = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
+// Function for Random Index Array
 function getRandomArrayElement(array) {
   var randomIndex = Math.floor(Math.random() * array.length);
   var randomElement = array[randomIndex]
   return randomElement
 }
 
-// function to collection user inputs
+// All logic in one single function
 function generatePassword() {
+  // Defining password length
   var passwordLength = parseInt(prompt("How many characters are required? (between 8 and 128 characters.)"), 10);
+  // Error message if not a number
   if (Number.isNaN(passwordLength)) {
     alert("must enter a number")
     return null;
   }
+  // Error message if wrong length
   if (passwordLength < 8 && passwordLength > 128) {
     alert("The password length must be between 8 and 128 characters.")
     return null;
   }
 
+  // Window pop up with characters selectors
   var hasSpecChars = confirm("Select ok to add spec chars to your password")
   var hasNums = confirm("Select ok to add numbers s to your password")
   var hasLowerCase = confirm("Select ok to add lower case to your password")
   var hasUpperCase = confirm("Select ok to add upper chars to your password")
   
+  // Defining the characters based on user selection. (Not sure why the "push" command didn't work instead concat)
   var potentialPasswordValues = []
   
   if (hasSpecChars) {
@@ -45,7 +51,9 @@ function generatePassword() {
     potentialPasswordValues = potentialPasswordValues.concat(uppercase);
   }
 
+  // Collecting the characters based on the password length and index array
   var password = "";
+  
   for (var i = 0; i < passwordLength; i++) {
     password = password + getRandomArrayElement(potentialPasswordValues);
   }
